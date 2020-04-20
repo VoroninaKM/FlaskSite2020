e7849b906c897a7f2a474e8e11137918812ef3c7
Voronina Ksenia <63294575+kseniavv@users.noreply.github.com>
2020-04-20 19:01:14 +0300

    Всё перенесла

---
+++ microblog/app/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask 
+app = Flask(__name__) 
+from app import routes
+
---
+++ microblog/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+

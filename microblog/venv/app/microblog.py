c475c99ca3669dccf829bd7e217a39486a96be5d
Voronina Ksenia <63294575+kseniavv@users.noreply.github.com>
2020-04-23 01:11:26 +0300

    sing and register

--- microblog/venv/app/microblog.py
+++ microblog/venv/app/microblog.py
@@ -2,5 +2,10 @@
 # coding: utf-8
 
 # In[ ]:
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
